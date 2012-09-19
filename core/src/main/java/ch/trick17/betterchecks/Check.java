ffed5b8b7e0225583f2331e92f29003c25d3b28a
Michael Faes <michael@trick17.ch>
2012-09-19 12:00:16 +0200

    Removed the simple checks - concentrate on the main API for now

--- core/src/main/java/ch/trick17/betterchecks/Check.java
+++ core/src/main/java/ch/trick17/betterchecks/Check.java
@@ -1,10 +1,5 @@
 package ch.trick17.betterchecks;
 
-import static ch.trick17.betterchecks.MsgFormatId.ARG_EMPTY;
-import static ch.trick17.betterchecks.MsgFormatId.ARG_NULL;
-import static ch.trick17.betterchecks.MsgFormatter.defaultArgName;
-import static ch.trick17.betterchecks.MsgFormatter.formatMsg;
-
 import java.util.Collection;
 
 import ch.trick17.betterchecks.fluent.CollectionCheck;
@@ -18,38 +13,6 @@ import ch.trick17.betterchecks.fluent.StringCheck;
 public class Check {
     
     private Check() {}
-    
-    /*
-     * Simple checks
-     */
-    
-    public static void that(final boolean condition, final String message) {
-        if(!condition)
-            throw new IllegalArgumentException(message);
-    }
-    
-    public static void notNull(final Object arg) {
-        if(arg == null)
-            throw new IllegalArgumentException(formatMsg(ARG_NULL,
-                    defaultArgName()));
-    }
-    
-    public static void notNull(final Object arg, final String argName) {
-        if(arg == null)
-            throw new IllegalArgumentException(formatMsg(ARG_NULL, argName));
-    }
-    
-    public static void notEmpty(final String arg) {
-        if(arg == null || arg.isEmpty())
-            throw new IllegalArgumentException(formatMsg(ARG_EMPTY,
-                    defaultArgName()));
-    }
-    
-    public static void notEmpty(final String arg, final String argName) {
-        if(arg == null || arg.isEmpty()) {
-            throw new IllegalArgumentException(formatMsg(ARG_EMPTY, argName));
-        }
-    }
     
     /*
      * Fluent checks
