d43bb31ad94163247411257cd9f30525cadbcbf8
Michael Faes <michael@trick17.ch>
2012-10-05 17:24:35 +0200

    Added javadoc to SimpleThreadLocal methods

--- core/src/main/java/ch/trick17/betterchecks/util/SimpleThreadLocal.java
+++ core/src/main/java/ch/trick17/betterchecks/util/SimpleThreadLocal.java
@@ -5,7 +5,7 @@ import java.lang.reflect.InvocationTargetException;
 
 /**
  * A {@link ThreadLocal} that uses the no-arg constructor of the class passed to
- * it at creation time to create {@link #initialValue()}s.
+ * it at creation time to create an {@link #initialValue()}.
  * 
  * @author Michael Faes
  * @param <T>
@@ -15,16 +15,49 @@ public final class SimpleThreadLocal<T> extends ThreadLocal<T> {
     
     private Constructor<? extends T> constructor;
     
+    /**
+     * Creates a simple thread local that uses the no-arg constructor of the
+     * given class to create an {@link #initialValue()}.
+     * 
+     * @param clazz
+     *            The class of the objects this thread local creates and holds.
+     *            Must not be an abstract class, an interface, an array class, a
+     *            primitive type or <code>void</code>, and must have a
+     *            accessible no-argument constructor.
+     * @throws IllegalArgumentException
+     *             if the no-arg constructor of the given type can not be
+     *             obtained.
+     */
     public SimpleThreadLocal(final Class<? extends T> clazz) {
         try {
             constructor = clazz.getConstructor();
         } catch(final NoSuchMethodException e) {
-            final String msg = "Could not access the no-arg constructor of class "
-                    + clazz.getName();
-            throw new RuntimeException(msg, e);
+            final String msg = "Class " + clazz.getName()
+                    + " does not have a no-arg constructor";
+            throw new IllegalArgumentException(msg, e);
         }
     }
     
+    /**
+     * Returns the current thread's "initial value" for this thread-local
+     * variable. This method will be invoked the first time a thread accesses
+     * the variable with the {@link #get} method, unless the thread previously
+     * invoked the {@link #set} method, in which case the
+     * <code>initialValue</code> method will not be invoked for the thread.
+     * Normally, this method is invoked at most once per thread, but it may be
+     * invoked again in case of subsequent invocations of {@link #remove}
+     * followed by {@link #get}.
+     * <p>
+     * This implementation returns a newly created object using the no-arg
+     * constructor of the class provided at construction time.
+     * 
+     * @throws RuntimeException
+     *             if the new instance can not be created. The cause of the
+     *             exception is one of the following:
+     *             {@link IllegalAccessException},
+     *             {@link InvocationTargetException} or
+     *             {@link InstantiationException}.
+     */
     @Override
     protected T initialValue() {
         try {
